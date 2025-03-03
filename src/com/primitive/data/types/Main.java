fae5f4692b3766e26472f1e623476f296090e022
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-03-04 00:48:00 +0100

    Add files via upload
    Commiting Main class with byte variable example

---
+++ src/com/primitive/data/types/Main.java
@@ -0,0 +1,27 @@
+package com.primitive.data.types;
+// Java program shows byte data type range
+// Class
+public class Main {
+
+    // MAin method to run java program
+    public static void main(String[] args) {
+        // Creating byte variable
+        byte my_byte = 126;
+
+        //Printing to console variable value
+        System.out.println("Variable = " + my_byte) ;
+
+        // Iterating my_byte
+        my_byte++;
+
+        //Printing to console variable value
+        System.out.println("Variable = " + my_byte);
+
+        // Iterating my_byte
+        my_byte++;
+
+        //Printing to console variable value
+        System.out.println("Variable = " + my_byte);
+
+    }
+}
